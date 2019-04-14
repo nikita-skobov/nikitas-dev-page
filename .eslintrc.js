68a878d00ce4de3227cbf50ac0dae156e73ba497
nikita-skobov <nskobov.websites@gmail.com>
2019-04-14 12:12:22 -0500

    removes proptypes rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
       "linebreak-style": ["off"],
       "react/jsx-filename-extension": ["off"],
       "react/jsx-one-expression-per-line": ["off"],
+      "react/prop-types": ["off"],
       "no-console": ["off"],
       "jsx-a11y/click-events-have-key-events": ["off"],
       "jsx-a11y/no-static-element-interactions": ["off"],
