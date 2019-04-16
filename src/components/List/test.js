1bd6ca2193aec3523844ccc15465ea627aabefc6
nikita-skobov <nskobov.websites@gmail.com>
2019-04-15 22:29:47 -0500

    adds test for list not being an array

--- src/components/List/test.js
+++ src/components/List/test.js
@@ -19,4 +19,17 @@ describe('the list component', () => {
     const divElm = wrapper.find('div')
     expect(divElm.exists()).toBeTruthy()
   })
+
+  it('should render a loading spinner at first', () => {
+    const wrapper = mount(<Provider store={store}><ConnectedList /></Provider>)
+    const htmlstring = wrapper.html()
+    expect(htmlstring).toContain('spinner-border')
+  })
+
+  it('should render an error if list is not an array', () => {
+    store = setupStore(undefined, { repoList: { list: undefined } })
+    const wrapper = mount(<Provider store={store}><ConnectedList /></Provider>)
+    const htmlstring = wrapper.html()
+    expect(htmlstring).not.toContain('spinner-border')
+  })
 })
