acf041800b9fb7c7506afd0303454823c7cca42e
nikita-skobov <nskobov.websites@gmail.com>
2019-04-16 12:08:51 -0500

    removes placeholder component

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,13 @@ import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 
 import './App.css'
-import Footer from './Footer'
-import List from './List'
+import ConnectedList from './List'
 
 export function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Footer} />
-        <Route path="/article/:id" component={List} />
+        <Route exact path="/" component={ConnectedList} />
       </Switch>
     </div>
   )
