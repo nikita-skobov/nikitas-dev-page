a06ae584e81d038576648ceed76bbc2d56a6c45c
nikita-skobov <nskobov.websites@gmail.com>
2019-04-24 11:21:11 -0500

    fixes tests to use Router as wrapper

--- src/components/ListItem/test.js
+++ src/components/ListItem/test.js
@@ -1,18 +1,25 @@
 /* global expect describe it beforeEach fetch */
 import * as React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 // eslint-disable-next-line
 import { mount } from 'enzyme'
+
 
 import ListItem from './index'
 
 describe('ListItem component', () => {
   it('should render', () => {
-    const wrapper = mount(<ListItem />)
+    const wrapper = mount(<Router><ListItem /></Router>)
     expect(wrapper.find('.list-item').exists()).toBeTruthy()
   })
 
-  it('should have a link', () => {
-    const wrapper = mount(<ListItem repoURL="https://github.com/nikita-skobov/backend-frontend-template" />)
-    expect(1).toBe(1)
+  it('should have a navlink to the repository component', () => {
+    const repoName = 'some-repo-name'
+    const wrapper = mount(
+      <Router>
+        <ListItem name={`${repoName}`} html_url="https://github.com/nikita-skobov/backend-frontend-template" />
+      </Router>,
+    )
+    expect(wrapper.html()).toMatch(`<a href="/repo/${repoName}">`)
   })
 })
