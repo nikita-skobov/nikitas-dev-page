31681501e6f88c5d404350f238825ef6f499edfc
nikita-skobov <nskobov.websites@gmail.com>
2019-04-24 15:09:19 -0500

    modifies setupStore function to optionally overwrite reducers

--- src/setupStore.js
+++ src/setupStore.js
@@ -2,8 +2,8 @@ import { applyMiddleware, createStore, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers/index'
 
-export function setupStore(allEnhancers = applyMiddleware(thunk), initialState = {}) {
-  return createStore(reducers, { ...initialState }, allEnhancers)
+export function setupStore(allEnhancers = applyMiddleware(thunk), initialState = {}, theReducers = reducers) {
+  return createStore(theReducers, { ...initialState }, allEnhancers)
 }
 
 export function createEnhancers(middlewareArray) {
