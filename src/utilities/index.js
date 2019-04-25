ddb6dfec38f6eee792174ff3e67f0b4473edbe68
nikita-skobov <nskobov.websites@gmail.com>
2019-04-24 19:29:52 -0500

    fixes Error throwing (forgot new)

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -4,7 +4,7 @@ export function flushAllPromises() {
 
 export function handleErrors(response) {
   if (!response.ok) {
-    throw Error(response.statusText)
+    throw new Error(response.statusText)
   }
   return response
 }
