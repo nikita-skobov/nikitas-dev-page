ca7e8f979b8ddcdab9fb217247eb3337ff552cb1
nikita-skobov <nskobov.websites@gmail.com>
2019-04-26 13:29:35 -0500

    fixes rendering logic based on number of children
    ie: if only one child, it should still work, and in order to render a divider between all children, it didnt need the index % 2 expression

--- src/components/GroupSpacer/index.js
+++ src/components/GroupSpacer/index.js
@@ -7,11 +7,19 @@ import { GROUP_SPACER_COMPONENT_CLASS_NAME } from '../../constants'
 export function GroupSpacer(props) {
   const { children } = props
 
+  if (!Array.isArray(children)) {
+    return (
+      <div className={GROUP_SPACER_COMPONENT_CLASS_NAME}>
+        {children}
+      </div>
+    )
+  }
+
   const lastBadgeIndex = children.length - 1
   const componentList = []
   children.forEach((child, index) => {
     componentList.push(child)
-    if (index % 2 === 0 && index !== lastBadgeIndex) {
+    if (index !== lastBadgeIndex) {
       // add a divider component between all the children
       // do not add a divider at the end of the list, though
       componentList.push(<div className="ns-divider" />)
