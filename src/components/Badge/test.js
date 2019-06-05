0b8fbe1253323f694c837ad4cbff145e787bea7a
nikita-skobov <nskobov.websites@gmail.com>
2019-06-05 12:32:34 -0500

    modifies tests for Badge component to work with new svg badges

--- src/components/Badge/test.js
+++ src/components/Badge/test.js
@@ -12,51 +12,16 @@ describe('badge component', () => {
     expect(wrapper.find(`.${BADGE_COMPONENT_CLASS_NAME}`).exists()).toBeTruthy()
   })
 
-  it('should have 2 buttons as children', () => {
+  it('should have a single img element', () => {
     const wrapper = mount(<Badge />)
-    const numButtons = wrapper.find('button').length
-    expect(numButtons).toBe(2)
+    const imgElement = wrapper.find('img')
+    expect(imgElement).toHaveLength(1)
   })
 
-  it('should use defaults for colorA, colorB and size props', () => {
+  it('should have the image source be an svg data uri', () => {
     const wrapper = mount(<Badge />)
-    const buttons = wrapper.find('button')
-    const button1 = buttons.get(0)
-    const button2 = buttons.get(1)
-
-    // first button should have colorA default: secondary
-    expect(button1.props.className).toMatch('secondary')
-    // first button should have size default: sm
-    expect(button1.props.className).toMatch('sm')
-
-    // second button should have colorB default: success
-    expect(button2.props.className).toMatch('primary')
-    // second button should have size default: sm
-    expect(button2.props.className).toMatch('sm')
-  })
-
-  it('should be able to override the defaults', () => {
-    const wrapper = mount(<Badge colorA="info" colorB="danger" size="lg" />)
-    const buttons = wrapper.find('button')
-    const button1 = buttons.get(0)
-    const button2 = buttons.get(1)
-
-    expect(button1.props.className).toMatch('info')
-    expect(button1.props.className).toMatch('lg')
-
-    expect(button2.props.className).toMatch('danger')
-    expect(button2.props.className).toMatch('lg')
-  })
-
-  it('should render textA in first button, textB in second button', () => {
-    const textA = 'dsadsadsa'
-    const textB = '3243erwrewrew'
-    const wrapper = mount(<Badge textA={textA} textB={textB} />)
-    const buttons = wrapper.find('button')
-    const button1 = buttons.get(0)
-    const button2 = buttons.get(1)
-
-    expect(button1.props.children).toBe(textA)
-    expect(button2.props.children).toBe(textB)
+    const imgElement = wrapper.find('img')
+    const { src } = imgElement.getElement().props
+    expect(src.substr(0, 18)).toEqual('data:image/svg+xml')
   })
 })
