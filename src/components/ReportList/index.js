6e60e9dcc60ae23c1e895dc2c2e51fa016f615c3
nikita-skobov <nskobov.websites@gmail.com>
2019-06-06 23:37:40 -0500

    adds svg arrow and reportTitle via enum

--- src/components/ReportList/index.js
+++ src/components/ReportList/index.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { ListGroup, ListGroupItem } from 'reactstrap'
+import { ListGroup, ListGroupItem, Spinner } from 'reactstrap'
 
+const reportTitles = {
+  1: <h3>This repository has not been configured to generate build reports</h3>,
+  2: <Spinner color="dark" />,
+  3: <h3>Build Reports</h3>,
+}
 
 export class ReportList extends Component {
   render() {
+    const { repoName, reportEnum } = this.props
+    const reportTitle = reportTitles[reportEnum]
+
+
     return (
-      <ListGroup>
-        <ListGroupItem>dsadsa</ListGroupItem>
-      </ListGroup>
+      <div>
+        {reportTitle}
+        <ListGroup>
+          <ListGroupItem>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 8 8">
+              <path d="M1.5 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z" transform="translate(1)" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 8 8">
+              <path d="M1.5 0l-1.5 1.5 4 4 4-4-1.5-1.5-2.5 2.5-2.5-2.5z" transform="translate(0 1)" />
+            </svg>
+          </ListGroupItem>
+        </ListGroup>
+      </div>
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps)
   const latestReport = ownProps.reportData
   const { repoName } = ownProps
 
@@ -23,15 +43,15 @@ const mapStateToProps = (state, ownProps) => {
   // const failedToFetch = state.reports[ownProps.name].failedToFetch
   const failedToFetch = false
 
-  let hasReportEnum = 0 // stilll fetching
+  let reportEnum = 2 // stilll fetching
   if (typeof latestReport === 'object') {
-    hasReportEnum = 1 // report fetched and exists
+    reportEnum = 3 // report fetched and exists
   } else if (failedToFetch) {
-    hasReportEnum = -1 // report doesnt exist for this repo
+    reportEnum = 1 // report doesnt exist for this repo
   }
 
   return {
-    hasReportEnum,
+    reportEnum,
     repoName,
     reportList: [
       latestReport,
