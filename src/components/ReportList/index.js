89a09082279381c7924cacf931131b2197950a06
nikita-skobov <nskobov.websites@gmail.com>
2019-06-10 12:16:16 -0500

    changes reportList to class component, adds showBuildsMultiplier functionality

--- src/components/ReportList/index.js
+++ src/components/ReportList/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { ListGroup, ListGroupItem } from 'reactstrap'
 
@@ -7,44 +7,67 @@ import { REPORT_EXIST, REPORT_NOT_FETCHED_YET } from '../../constants'
 
 const SHOW_BUILDS_MAX = 10
 
-export function ReportList(props) {
-  const { reportData, fetchReportCallback, repoName } = props
+export class ReportList extends Component {
+  constructor(props) {
+    super(props)
 
-  if (!reportData) return null
+    this.state = {
+      showBuildsMultiplier: 1,
+    }
 
-  const latest = reportData.latest.data
-  const buildNumber = parseInt(latest.build_number, 10)
-
-  const list = [
-    <ReportItem isLatest reportStatus={REPORT_EXIST} data={latest} repoName={repoName} buildNumber={buildNumber} fetchReportCallback={fetchReportCallback} />,
-  ]
-
-  let stopAt = buildNumber - SHOW_BUILDS_MAX
-  if (stopAt < 0) stopAt = 0
-
-  for (let i = buildNumber - 1; i > stopAt; i -= 1) {
-    const dataObj = reportData[i] ? reportData[i].data : undefined
-    const status = reportData[i] ? reportData[i].reportStatus : REPORT_NOT_FETCHED_YET
-
-    list.push(
-      <ReportItem reportStatus={status} data={dataObj} repoName={repoName} buildNumber={i} fetchReportCallback={fetchReportCallback} />,
-    )
+    this.loadMore = this.loadMore.bind(this)
   }
 
-  // this is a button that loads more report items
-  list.push(
-    <ListGroup>
-      <ListGroupItem action disabled active>
-        Load More
-      </ListGroupItem>
-    </ListGroup>
-  )
+  loadMore() {
+    this.setState((prevState) => {
+      const tempState = prevState
+      tempState.showBuildsMultiplier += 1
+      return tempState
+    })
+  }
 
-  return (
-    <div>
-      {list}
-    </div>
-  )
+  render() {
+    const { reportData, fetchReportCallback, repoName } = this.props
+
+    const { showBuildsMultiplier } = this.state
+
+    if (!reportData) return null
+
+    const latest = reportData.latest.data
+    const buildNumber = parseInt(latest.build_number, 10)
+
+    const list = [
+      <ReportItem isLatest reportStatus={REPORT_EXIST} data={latest} repoName={repoName} buildNumber={buildNumber} fetchReportCallback={fetchReportCallback} />,
+    ]
+
+    let stopAt = buildNumber - (SHOW_BUILDS_MAX * showBuildsMultiplier)
+    if (stopAt < 0) stopAt = 0
+
+    for (let i = buildNumber - 1; i > stopAt; i -= 1) {
+      const dataObj = reportData[i] ? reportData[i].data : undefined
+      const status = reportData[i] ? reportData[i].reportStatus : REPORT_NOT_FETCHED_YET
+
+      list.push(
+        <ReportItem reportStatus={status} data={dataObj} repoName={repoName} buildNumber={i} fetchReportCallback={fetchReportCallback} />,
+      )
+    }
+
+    // this is a button that loads more report items
+    list.push(
+      <ListGroup onClick={this.loadMore}>
+        <ListGroupItem action disabled active>
+          Load More
+        </ListGroupItem>
+      </ListGroup>,
+    )
+
+    return (
+      <div>
+        {list}
+      </div>
+    )
+  }
 }
+
 
 export default ReportList
